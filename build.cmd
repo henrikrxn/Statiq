6ed184b9b8144ef0a6495190a962c00330848642
Dave Glick <dave@daveaglick.com>
2021-12-30 14:23:13 -0500

    Updating all the submodules

--- build.cmd
+++ build.cmd
@@ -6,15 +6,15 @@ cd "Statiq.Framework\build\Statiq.Framework.Build"
 dotnet run -- %*
 cd %~dp0
 
-echo Timeout for 5 minutes to wait for NuGet indexing
-timeout /t 300
+echo Timeout for 10 minutes to wait for NuGet indexing
+timeout /t 600
 
 cd "Statiq.Web\build\Statiq.Web.Build"
 dotnet run -- %*
 cd %~dp0
 
-echo Timeout for 5 minutes to wait for NuGet indexing
-timeout /t 300
+echo Timeout for 10 minutes to wait for NuGet indexing
+timeout /t 600
 
 cd "Statiq.Docs\build\Statiq.Docs.Build"
 dotnet run -- %*
